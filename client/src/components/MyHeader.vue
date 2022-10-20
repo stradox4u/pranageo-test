ae7f9dfa5ef32905513f78f9d8e59b7b9b5af879
Umar Adejoh <stradox@gmail.com>
2022-10-20 01:05:25 +0100

    created route and logic to reconstitute stages json and return to client, moved header in client to separate component

---
+++ client/src/components/MyHeader.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div class="w-full bg-gold h-[36px] px-8 py-1">
+    <RouterLink :to="{ name: home }">
+      <h1 class="font-montserrat font-bold text-xl">PRANAGEO Test</h1>
+    </RouterLink>
+  </div>
+</template>
